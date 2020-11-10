a22a97eedad18f9da180e19660c8ca189a882a65
M-Nurda <trim.arts007@gmail.com>
2020-11-10 13:59:30 +0600

    Added Title and Icon.

--- spacebattle/general.py
+++ spacebattle/general.py
@@ -1,12 +1,24 @@
 import pygame
 
+# Initialize the game
 pygame.init()
-size = (800, 600)
-fps = 20 # Frames per second
-screen = pygame.display.set_mode(size)
+
+# Frames per second
+fps = 20
+
+# Create the screen
+screen = pygame.display.set_mode((800,600))
+
+# Title and Icon
+pygame.display.set_caption("Space Battle")
+icon = pygame.image.load('project.png')
+pygame.display.set_icon(icon)
 
 run=True
 while run:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run=False
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill((0,255,255))
+    pygame.display.update()
